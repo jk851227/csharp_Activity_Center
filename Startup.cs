a04b9aa200b4f7efffc4ff7a6d9bcc56226ac65a
Joseph Kim <jk851227@gmail.com>
2020-08-21 14:35:02 -0700

    Added .GetConnectionString

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace ActivityCenter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ActivityCenterContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
+            services.AddDbContext<ActivityCenterContext>(options => options.UseMySql(Configuration.GetConnectionString("DBInfo:ConnectionString")));
             // to access session directly from view: @using Microsoft.AspNetCore.Http in Views/_ViewImports.cshtml
             services.AddHttpContextAccessor();
             services.AddSession();
